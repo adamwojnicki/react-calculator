94928389d479bf8e3242799ac7b1c4213b1200d2
Adam Wojnicki <wojnicki.adam1@gmail.com>
2019-07-31 20:16:36 +0200

    Made .scss files and images working correctly

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,25 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
+                use: ['babel-loader']
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[hash].[ext]',
+                        outputPath: 'imgs'
+                    }
+                }
             }
         ]
     },
