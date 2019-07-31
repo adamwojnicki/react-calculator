8fc8970e19277e7cf153e2938b90f77f4554a356
Adam Wojnicki <wojnicki.ada1@gmial.com>
2019-07-31 12:12:59 +0200

    Initial commit. Environment setup.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            App goes here...
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
