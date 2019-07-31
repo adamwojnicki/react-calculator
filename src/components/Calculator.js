9146f1fa26fa0bd0593cf2ced9c9672c00691a11
Adam Wojnicki <wojnicki.adam1@gmail.com>
2019-07-31 19:16:38 +0200

    Commit #01: Made style loaders working + created Calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calculator = () => {
+    return (
+        <div>
+            Calculator goes here
+        </div>
+    )
+}
+
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+
+import Calculator from './components/Calculator';
 
 const App = () => {
     return (
-        <div>
-            App goes here...
+        <div className='container'>
+            <Calculator />
         </div>
     )
 }
