e848252ce5569867ac4a863ef085294664b1b7ae
Adam Wojnicki <wojnicki.ada1@gmial.com>
2019-08-01 12:37:40 +0200

    Commit #02: Working on UI. Created Key component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import Key from './Key';
 
 const Calculator = () => {
     return (
-        <div>
-            Calculator goes here
+        <div className='calculator-face'>
+            <div className='calc-output'>
+                <p>10101</p>
+            </div>
+            <div className='calc-input'>
+                <input type='text'></input>
+            </div>
+            <div className='key-container'>
+                <Key type='operator' value='C'/>
+                <Key type='operator' value='+/-'/>
+                <Key type='operator' value='%'/>
+                <Key type='operator' value='&divide;'/>
+                <Key type='digit' value='1'/>
+                <Key type='digit' value='2'/>
+                <Key type='digit' value='3'/>
+                <Key type='operator' value='&times;'/>
+                <Key type='digit' value='4'/>
+                <Key type='digit' value='5'/>
+                <Key type='digit' value='6'/>
+                <Key type='operator' value='&minus;'/>
+                <Key type='digit' value='7'/>
+                <Key type='digit' value='8'/>
+                <Key type='digit' value='9'/>
+                <Key type='operator' value='+'/>
+                <Key type='digit' value='0'/>
+                <Key type='operator' value=','/>
+                <Key type='operator' value='DEL'/>
+                <Key type='operator' value='='/>
+            </div>
         </div>
     )
 }
---
+++ src/components/Key.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Key = (props) => {
+    return (
+        <div className='key'>
+            {props.value}
+        </div>
+    )
+}
+
+export default Key;
